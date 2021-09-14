896b04f0d0459c42ea92a65be8860992b03ade45
FahriDarmawan <44422245+FahriDarmawan@users.noreply.github.com>
2021-09-14 14:28:37 +0700

    2DEndlessRunner

---
+++ 2D Endless Runner/Assets/Scripts/CharacterSoundController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSoundController : MonoBehaviour
+{
+
+    public AudioClip jump;
+    public AudioClip scoreHighlight;
+    public AudioClip gameOver;
+
+    private AudioSource audioPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayJump()
+    {
+        audioPlayer.PlayOneShot(jump);
+    }
+
+    public void PlayScoreHighlight()
+    {
+        audioPlayer.PlayOneShot(scoreHighlight);
+    }
+
+    public void PlayGameOver()
+    {
+        audioPlayer.PlayOneShot(gameOver);
+    }
+}
